ad4aec559692b02d85d688f04936a7f08d3a0e1f
qiongchaoli <qiongchaoli@haowaihao.com.cn>
2020-07-31 13:02:24 +0800

    大球吃小球，完成

--- src/base15/day10/ball3.py
+++ src/base15/day10/ball3.py
@@ -60,56 +60,41 @@ class Ball(object):
                 other.alive = False
                 self.radius = self.radius + int(other.radius * 0.146)
 
-    def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius, 0)
-
-
-
 def main():
-    # 定义用来装所有球的容器
-    balls = []
     # 初始化 pygame中的模块
     pygame.init()
     # 初始化显示的窗口并设置窗口尺寸
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption('大球吃小球')
 
+    # 绘制一个圆(参数分别是: 屏幕, 颜色, 圆心位置, 半径, 0表示填充圆)
+    # pygame.draw.circle(screen, (255, 0, 0), (100, 100), 30, 0)
+    # 加载图片
+    # ball_image = pygame.image.load('./res/ball.png')
+    # 在窗口上渲染图像
+    # screen.blit(ball_image, (50, 50))
+    # 刷新当前窗口(渲染窗口将绘制的图像呈现出来)
+    # pygame.display.flip()
+
+    # 定义变量来表示小球在屏幕上的位置
+    x, y = 50, 50
+
     running = True
     # 开启一个事件的循环处理
     while running:
         # 从消息队列中获取事件并对事件进行处理
         for event in pygame.event.get():
-            # 点右上角退出键，退出游戏
             if event.type == pygame.QUIT:
-                print('接收到退出')
                 running = False
-            # 处理鼠标事件
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                # 获取鼠标的位置
-                x, y = event.pos
-                radius = randint(10, 100)
-                sx, sy = randint(-10, 10), randint(-10, 10)
-                color = Color.random_color()
-                # 在点击鼠标的位置创建一个球（大小，速度和颜色随机）
-                ball = Ball(x, y, radius, sx, sy, color)
-                # 将球添加到列表容器中
-                balls.append(ball)
         # 设置窗口的背景色(颜色是由红绿蓝三原色构成的元组)
         screen.fill((255, 255, 255))
-        # 取出容器的球，如果没被吃掉就绘制，被吃掉就移除
-        for ball in balls:
-            if ball.alive:
-                ball.draw(screen)
-            else:
-                balls.remove(ball)
+        pygame.draw.circle(screen, (255, 0, 0), (x, y), 30, 0)
+
+        # 刷新当前窗口(渲染窗口将绘制的图像呈现出来)
         pygame.display.flip()
-        # 每隔50毫秒就改变球的位置再刷新容器
+        # 每隔50毫秒就改变小球的位置再刷新窗口
         pygame.time.delay(50)
-        for ball in balls:
-            ball.move(screen)
-            # 检查球有没有吃到其它球
-            for other in balls:
-                ball.eat(other)
+        x, y = x + 5, y + 5
 
 
 if __name__ == '__main__':
