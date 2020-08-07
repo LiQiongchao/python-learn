b0adb597d91363cc0a6e80efb52caa2d4f43b435
qiongchaoli <qiongchaoli@haowaihao.com.cn>
2020-08-07 17:16:09 +0800

    http请求测试

--- src/base75/take_data_parse.py
+++ src/base75/take_data_parse.py
@@ -7,6 +7,8 @@
 
 import requests
 
+
+print('----------------------------------GET------------------------------------------')
 # GET 请求
 resp = requests.get('http://www.baidu.com/index.html')
 print(resp.status_code)
@@ -15,11 +17,39 @@ print(resp.cookies)
 print(resp.content.decode('utf-8'))
 
 
-print('----------------------------------------------------------------------------')
+print('----------------------------------POST------------------------------------------')
 # POST 请求
 resp = requests.post('http://httpbin.org/post', data={'name': 'li', 'age': 32})
 # print(resp.content)
 print(resp.text)
 data = resp.json()
 print(type(data))
+
+
+print('----------------------------------GET WITH HEADERS------------------------------------------')
+resp = requests.get(url='https://movie.douban.com/top250',
+             headers={
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/83.0.4103.97 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;'
+                          'q=0.9,image/webp,image/apng,*/*;'
+                          'q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+             })
+print(resp.status_code)
+# print(resp.text)
+
+
+print('----------------------------------upload file------------------------------------------')
+resp = requests.post(url='http://httpbin.org/post', files={'file': open('match_str.py', 'rb')})
+print(resp.text)
+
+
+print('----------------------------------operate cookie------------------------------------------')
+
+
+
+
+
 
