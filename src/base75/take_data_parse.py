4e2c90d9f91b7130c97b41fe54193bb23e91dd70
qiongchaoli <qiongchaoli@haowaihao.com.cn>
2020-08-07 17:25:41 +0800

    http cookies

--- src/base75/take_data_parse.py
+++ src/base75/take_data_parse.py
@@ -7,7 +7,6 @@
 
 import requests
 
-
 print('----------------------------------GET------------------------------------------')
 # GET 请求
 resp = requests.get('http://www.baidu.com/index.html')
@@ -15,7 +14,6 @@ print(resp.status_code)
 print(resp.headers)
 print(resp.cookies)
 print(resp.content.decode('utf-8'))
-
 
 print('----------------------------------POST------------------------------------------')
 # POST 请求
@@ -25,18 +23,17 @@ print(resp.text)
 data = resp.json()
 print(type(data))
 
-
 print('----------------------------------GET WITH HEADERS------------------------------------------')
 resp = requests.get(url='https://movie.douban.com/top250',
-             headers={
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) '
-                          'AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/83.0.4103.97 Safari/537.36',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;'
-                          'q=0.9,image/webp,image/apng,*/*;'
-                          'q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-             })
+                    headers={
+                        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) '
+                                      'AppleWebKit/537.36 (KHTML, like Gecko) '
+                                      'Chrome/83.0.4103.97 Safari/537.36',
+                        'Accept': 'text/html,application/xhtml+xml,application/xml;'
+                                  'q=0.9,image/webp,image/apng,*/*;'
+                                  'q=0.8,application/signed-exchange;v=b3;q=0.9',
+                        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+                    })
 print(resp.status_code)
 # print(resp.text)
 
@@ -45,8 +42,17 @@ print('----------------------------------upload file----------------------------
 resp = requests.post(url='http://httpbin.org/post', files={'file': open('match_str.py', 'rb')})
 print(resp.text)
 
-
 print('----------------------------------operate cookie------------------------------------------')
+cookies = {"cookie1": "val1", "cookie2": "val2", "cookie3": "val3"}
+resp = requests.get('http://httpbin.org/cookies', cookies=cookies)
+print(resp.text)
+
+cookies_jar = requests.cookies.RequestsCookieJar()
+cookies_jar.set('cookie4', 'val4', domain='httpbin.org', path='/cookies')
+cookies_jar.set('cookie5', 'val5', domain='httpbin.org', path='/elsewhere')
+resp = requests.get('http://httpbin.org/cookies', cookies = cookies_jar)
+print(resp.text)
+
 
 
 
