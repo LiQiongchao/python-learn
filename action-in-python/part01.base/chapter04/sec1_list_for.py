06211feedcbd312efe62390933915bb3aca60b58
liqc <liqiongchao2012@163.com>
2024-01-17 23:15:16 +0800

    feat: 遍历列表

---
+++ action-in-python/part01.base/chapter04/sec1_list_for.py
@@ -0,0 +1,19 @@
+# 列表遍历
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    # print(magician)  # 打印每一条
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+print("Thank you,everyone. That was a great magic show!")
+
+
+## 练习
+pizzas = ['榴莲比萨', '鸡肉比萨', '火腿比萨']
+for pizza in pizzas:
+    print(f"I like {pizza}")
+print("I really love pizza！")
+
+animals = ['dog', 'cat', 'tigger', 'duck']
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+print("Any of these animals would make a great pet!")
