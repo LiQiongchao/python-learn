2fa1fdf5ebf36f6a7e47331b6902aee696862071
LiQiongchao <liqiongchao2012@163.com>
2024-01-25 11:54:41 +0800

    feat: 随机数

---
+++ test/__init__.py
@@ -0,0 +1,5 @@
+"""
+@Auther: LiQiongchao
+@Date: 2024-1-25
+@Description: 测试目录
+"""
---
+++ test/basetest/test01_random.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Author  : Liqc
+@Date    : 2024/1/25
+@Description: 随机数查询
+"""
+import random
+from datetime import time
+
+numbers = [i for i in range(21)]
+
+choices = random.choices(numbers, k=19)  # 随机抽取19个数
+print(choices)  # 数据会有重复
+
+sample = random.sample(numbers, k=19)  # 随机抽取19个数
+print(sample)  # 数据不会有重复
+
