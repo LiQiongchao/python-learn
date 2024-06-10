e13411e6b206f3f0fb902e9906e3b4cf503170d0
liqc <liqiongchao2012@163.com>
2024-06-10 23:24:29 +0800

    feat: function

---
+++ action-in-python/part01.base/chapter08/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Author  : Liqc
+@File    : __init__.py.py
+@Date    : 2024/6/10
+@Version : v1.0.0
+@Description: 函数
+"""
+
+if __name__ == '__main__':
+    pass
---
+++ action-in-python/part01.base/chapter08/sec1_definition.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Author  : Liqc
+@File    : sec1_definition.py
+@Date    : 2024/6/10
+@Version : v1.0.0
+@Description: 函数的定义
+"""
+
+
+def greet_user():  # 函数定义
+    """问候"""  # 文档注释，多行注释
+    print("hello")
+
+
+if __name__ == '__main__':
+    greet_user()  # 调用方法
